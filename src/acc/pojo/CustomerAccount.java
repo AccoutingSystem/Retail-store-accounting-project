9913d1ac87a0180af59a37c74d4d92cf40335a82
kebogile <kebzozo@webmail.co.za>
2015-01-30 10:24:46 +0200

    customer and customerAccount classes

--- src/acc/pojo/CustomerAccount.java
+++ src/acc/pojo/CustomerAccount.java
@@ -2,6 +2,14 @@ package acc.pojo;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
 @Entity
 @Table(name = "Account")
 public class CustomerAccount {
@@ -25,15 +33,21 @@ public class CustomerAccount {
 
 	@Column(name = "InterestRate")
 	private String interestRate;
+	
+	//@Column(name = "CustomerID")
+	@OneToOne()
+	@PrimaryKeyJoinColumn
+	private Customer customer;
+	
 
 	public CustomerAccount() {
 		super();
 	}
 
-	public CustomerAccount(int accID, String custbalance, String installment,
+	public CustomerAccount(String custbalance, String installment,
 			String custCredit, Date date, String interestRate) {
 		super();
-		this.accID = accID;
+		
 		this.custbalance = custbalance;
 		this.installment = installment;
 		this.custCredit = custCredit;
@@ -41,53 +55,77 @@ public class CustomerAccount {
 		this.interestRate = interestRate;
 	}
 
-	public int getAccID() {
-		return accID;
-	}
-
-	public void setAccID(int accID) {
-		this.accID = accID;
+	public CustomerAccount( String custbalance, String installment,
+			String custCredit, Date date, String interestRate, Customer customer) {
+		super();
+		
+		this.custbalance = custbalance;
+		this.installment = installment;
+		this.custCredit = custCredit;
+		this.date = date;
+		this.interestRate = interestRate;
+		this.customer = customer;
 	}
 
 	public String getCustbalance() {
 		return custbalance;
 	}
 
+
 	public void setCustbalance(String custbalance) {
 		this.custbalance = custbalance;
 	}
+
 
 	public String getInstallment() {
 		return installment;
 	}
 
+
 	public void setInstallment(String installment) {
 		this.installment = installment;
 	}
+
 
 	public String getCustCredit() {
 		return custCredit;
 	}
 
+
 	public void setCustCredit(String custCredit) {
 		this.custCredit = custCredit;
 	}
+
 
 	public Date getDate() {
 		return date;
 	}
 
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
+
 
 	public String getInterestRate() {
 		return interestRate;
 	}
 
+
 	public void setInterestRate(String interestRate) {
 		this.interestRate = interestRate;
 	}
+
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
 	
 
 }
