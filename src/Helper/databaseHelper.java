cb7fffbbe4d388317f15b436dceaff604b39a8a9
sgco <nozewug@gmail.com>
2015-01-30 08:47:34 +0000

    persistence.xml

---
+++ src/Helper/databaseHelper.java
@@ -0,0 +1,32 @@
+package Helper;
+
+
+
+	
+	
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class databaseHelper {
+	
+	private static final EntityManagerFactory entityManagerFactory = produceObj();
+	private static EntityManagerFactory produceObj() {	
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("accounting");	
+		return entityManagerFactory;
+	}
+	public static EntityManagerFactory provideFactory(){	
+		return entityManagerFactory;	
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
