cb0ace26c3a33ae9e34e89fee5259f287d56ee71
kebogile <kebzozo@webmail.co.za>
2015-02-02 14:53:06 +0200

    created class Emloyee

---
+++ src/acc/Test/Test.java
@@ -0,0 +1,55 @@
+package acc.Test;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import acc.pojo.Customer;
+import acc.pojo.CustomerAccount;
+
+
+public class Test 
+{
+	
+	public static void main(String[] args) 
+	{
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("accounting");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		
+		//instantiating and object for AccountingDAO
+		//CustomerDAO dao = new CustomerDAO();
+		
+		Date date = new Date();
+		date.setMonth(1);;
+		
+	
+		Customer cust1 = new Customer("John","McRoy","25 Ben Street, Cape Town","021 569 2369","Developer",
+				"Female",1233456789,15000.00,5000.00,33550,"Capitec","kkk@gmail.com");
+		
+		Customer cust2 = new Customer("Kebo","Moreng","15 Lol Street, Johannesburg","011 253 5632","Cashier",
+				"Male",1233456789,15000.00,5000,35000,"Standard Bank","123@gmail.com");
+		
+		Customer cust3 = new Customer("Boby","Love","Kenny Street, Kimberley","021 569 2369","Manager",
+				"Female",1233456789,15000.00,5000,35000,"Absa","wer@gmail.com");
+		
+		
+				
+		CustomerAccount cAccount1 = new CustomerAccount(10000.00,520,5000,date,0.15,cust1);
+		CustomerAccount cAccount2 = new CustomerAccount(2000.00,200,893,date,0.15,cust2);
+		CustomerAccount cAccount3 = new CustomerAccount(4000,600,5630,date,0.15,cust3);
+		
+		em.persist(cust1);
+		em.persist(cust2);
+		em.persist(cust3);
+		
+		em.persist(cAccount1);
+		em.persist(cAccount2);
+		em.persist(cAccount3);
+		
+		em.getTransaction().commit();
+	}
+		
+}
--- src/acc/pojo/Employee.java
+++ src/acc/pojo/Employee.java
@@ -1,9 +1,299 @@
 package acc.pojo;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+@Table(name = "Employee")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Employee {
-//this is the employee class
-	//newest changes
-	//booby is back
+	@XmlAttribute
+	@Id
+	@GeneratedValue
+	@Column(name = "EmployeeID")
+	private long empID;
+	
+	@XmlAttribute
+	@Column(name = "EmployeeName")
+	private String empName;
+	
+	@XmlAttribute
+	@Column(name = "EmployeeSurname")
+	private String empSurname;
+	
+	@XmlAttribute
+	@Column(name = "EmployeeAddress")
+	private String empAddress;
+	
+	@XmlAttribute
+	@Column(name = "ContactNr")
+	private String empContactNr;
+	
+	@XmlAttribute
+	@Column(name = "JobTitle")
+	private String jobTitle;
+	
+	@XmlAttribute
+	@Column(name = "StartDate")
+	private Date startDate;
+	
+	@XmlAttribute
+	@Column(name = "IDNumber")
+	private long idNumber;
+
+	@XmlAttribute
+	@Column(name = "Gender")
+	private String gender;
+
+	@XmlAttribute
+	@Column(name = "Race")
+	private String race;
+
+	@XmlAttribute
+	@Column(name = "Email")
+	private String email;
+
+	@XmlAttribute
+	@Column(name = "Password")
+	private String password;
+
+	@XmlAttribute
+	@Column(name = "TaxNumber")
+	private String taxNumber;
+
+	@XmlAttribute
+	@Column(name = "Qualification")
+	private String qualification;
+
+	@XmlAttribute
+	@Column(name = "AccountNr")
+	private long empAccNr;
+
+	@XmlAttribute
+	@Column(name = "EmployeeName")
+	private long brachCode;
+
+	@XmlAttribute
+	@Column(name = "EmployeeName")
+	private String BranchName;
+
+	@ManyToOne()
+	@JoinColumn(name = "PayrollNumber")
+	private PayrolExpenses payrolls;
+	
+	@OneToMany()
+	@JoinColumn(name = "StoreAccNr")
+	private StoreAccountsPayable storeAccounts;
+	
+	@OneToMany()
+	@JoinColumn(name = "")
+	private StoreSalesRecord storeSalesRec;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String empName, String empSurname, String empAddress,
+			String empContactNr, String jobTitle, Date startDate,
+			long idNumber, String gender, String race, String email,
+			String password, String taxNumber, String qualification,
+			long empAccNr, long brachCode, String branchName,
+			PayrolExpenses payrolls, StoreAccountsPayable storeAccounts,
+			StoreSalesRecord storeSalesRec) {
+		super();
+		this.empName = empName;
+		this.empSurname = empSurname;
+		this.empAddress = empAddress;
+		this.empContactNr = empContactNr;
+		this.jobTitle = jobTitle;
+		this.startDate = startDate;
+		this.idNumber = idNumber;
+		this.gender = gender;
+		this.race = race;
+		this.email = email;
+		this.password = password;
+		this.taxNumber = taxNumber;
+		this.qualification = qualification;
+		this.empAccNr = empAccNr;
+		this.brachCode = brachCode;
+		BranchName = branchName;
+		this.payrolls = payrolls;
+		this.storeAccounts = storeAccounts;
+		this.storeSalesRec = storeSalesRec;
+	}
+
+	public long getEmpID() {
+		return empID;
+	}
+
+	public void setEmpID(long empID) {
+		this.empID = empID;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getEmpSurname() {
+		return empSurname;
+	}
+
+	public void setEmpSurname(String empSurname) {
+		this.empSurname = empSurname;
+	}
+
+	public String getEmpAddress() {
+		return empAddress;
+	}
+
+	public void setEmpAddress(String empAddress) {
+		this.empAddress = empAddress;
+	}
+
+	public String getEmpContactNr() {
+		return empContactNr;
+	}
+
+	public void setEmpContactNr(String empContactNr) {
+		this.empContactNr = empContactNr;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public long getIdNumber() {
+		return idNumber;
+	}
+
+	public void setIdNumber(long idNumber) {
+		this.idNumber = idNumber;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getRace() {
+		return race;
+	}
+
+	public void setRace(String race) {
+		this.race = race;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getTaxNumber() {
+		return taxNumber;
+	}
+
+	public void setTaxNumber(String taxNumber) {
+		this.taxNumber = taxNumber;
+	}
+
+	public String getQualification() {
+		return qualification;
+	}
+
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+
+	public long getEmpAccNr() {
+		return empAccNr;
+	}
+
+	public void setEmpAccNr(long empAccNr) {
+		this.empAccNr = empAccNr;
+	}
+
+	public long getBrachCode() {
+		return brachCode;
+	}
+
+	public void setBrachCode(long brachCode) {
+		this.brachCode = brachCode;
+	}
+
+	public String getBranchName() {
+		return BranchName;
+	}
+
+	public void setBranchName(String branchName) {
+		BranchName = branchName;
+	}
+
+	public PayrolExpenses getPayrolls() {
+		return payrolls;
+	}
+
+	public void setPayrolls(PayrolExpenses payrolls) {
+		this.payrolls = payrolls;
+	}
+
+	public StoreAccountsPayable getStoreAccounts() {
+		return storeAccounts;
+	}
+
+	public void setStoreAccounts(StoreAccountsPayable storeAccounts) {
+		this.storeAccounts = storeAccounts;
+	}
+
+	public StoreSalesRecord getStoreSalesRec() {
+		return storeSalesRec;
+	}
+
+	public void setStoreSalesRec(StoreSalesRecord storeSalesRec) {
+		this.storeSalesRec = storeSalesRec;
+	}
 	
 	
 }
