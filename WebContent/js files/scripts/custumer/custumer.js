b3bf8fa97e906ba03036437541cda72e9e9d037e
Moheberia <sebatimg@gmail.com>
2015-02-04 14:27:54 +0200

    client side scripts updates

--- WebContent/js files/scripts/custumer/custumer.js
+++ WebContent/js files/scripts/custumer/custumer.js
@@ -1,18 +1,19 @@
 var custumerModel = function()
 {
     var self = this;
-    this.custName = ko.observable("");
-    this.custSurname = ko.observable("");
-    this.custAddress = ko.observable("");
-    this.contactNr = ko.observable("");
-    this.occupation = ko.observable("");
-    this.gender = ko.observable("");
-    this.idNr = ko.observable("");
-    this.income = ko.observable(0.0);
-    this.monthlyExpenses = ko.observable(0.0);
-    this.branchCode = ko.observable(0);
-    this.bankName = ko.observable("");
-    this.email = ko.observable("");
+    self.listOfCustumers = ko.observableArray([]);
+    self.custName = ko.observable("");
+    self.custSurname = ko.observable("");
+    self.custAddress = ko.observable("");
+    self.contactNr = ko.observable("");
+    self.occupation = ko.observable("");
+    self.gender = ko.observable("");
+    self.idNr = ko.observable("");
+    self.income = ko.observable(0.0);
+    self.monthlyExpenses = ko.observable(0.0);
+    self.branchCode = ko.observable(0);
+    self.bankName = ko.observable("");
+    self.email = ko.observable("");
      
     var custumer = {
         //email: self.email,
@@ -42,7 +43,19 @@ var custumerModel = function()
             success: function(status,http)
             {
             	 alert(" :bingo.. ");
-            	 // code to empty to properties here
+            	 // empty the properties
+            	 	self.custName("");
+            	    self.custSurname("");
+            	    self.custAddres("");
+            	    self.contactNr("");
+            	    self.occupation("");
+            	    self.gender("");
+            	    self.idNr("");
+            	    self.income(0.0);
+            	    self.monthlyExpenses(0.0);
+            	    self.branchCode;
+            	    self.bankName("");
+            	    self.email("");
 
             },
             error: function(status,XHR)
@@ -51,20 +64,17 @@ var custumerModel = function()
                 alert(XHR.status + " : "+ " status" + status);
             }
         });
+        
+        // Insert Search code here
     };
     
-    /*public void update( 
-		
-		  
-			 String email)*/
    
     // update custumer infor
-    self.update = function()
+    self.updateCustumerInfor = function()
     {
         $.ajax({
             type: 'POST',
             url: "http://localhost:8080/AccountingERPSystem/rest?custId="+self.custId()+"&custName="+self.custName()+"&custSurname="+self.custSurname()+"&custAddress="+self.custAddress()+"&contactNr="+self.contactNr()+"&occupation="+self.occupation()+"&gender="+self.gender()+"&idNr="+self.idNr()+"&income="+self.income()+"&monthlyExpenses="+self.monthlyExpenses()+"&branchCode="+self.branchCode()+"&bankName="+self.bankName()+"&email="+self.email(),
-            data: ko.toJSON(userObject),
             contentType: 'application/json',
             dataType: "json",
             success: function()
@@ -80,7 +90,7 @@ var custumerModel = function()
     };
     
     // delete custumer
-    self.delete_ = function() {
+    self.deleteCustumer = function() {
 
         $.ajax({
             url: "http://localhost:8080/AccountingERPSystem/rest?idNr="+self.idNr(),
@@ -95,6 +105,25 @@ var custumerModel = function()
                 function(xhr, textStatus, err) {
                     alert(err);
                 });
+    };
+    // retrive  custumer list
+    self.custumerList = function()
+    {
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:8080/AccountingERPSystem/rest/',
+            contentType: 'application/json',
+            dataType: "json",
+            success: function(data)
+            {
+               self.listOfCustumers(data);
+
+            },
+            error: function()
+            {
+                alert("error has occured");
+            }
+        });
     };
     
 };
---
+++ WebContent/js files/scripts/custumer/custumerPayments.js
@@ -0,0 +1,11 @@
+var custumerPayments = function()
+{
+    var self = this;
+  
+};
+
+$(document).ready(function()
+{
+    ko.applyBindings(new custumerPayments());
+});
+
