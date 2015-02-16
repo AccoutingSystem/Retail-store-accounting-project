9635e1ca3170d67f02238d6b33cc97775a8f2440
bobeezy <bobymangoua@gmail.com>
2015-02-16 11:45:41 +0200

    Updated top panel, color, flag and removed useless forms.

--- WebContent/client side template/default/jsfiles/scripts/chartAccount/chart.js
+++ WebContent/client side template/default/jsfiles/scripts/chartAccount/chart.js
@@ -8,17 +8,19 @@ function createChartList(charts) {
 {
     var self = this;
     self.chartList = ko.observableArray([]);
-    //self.type = [{ name: 'Payable'},{name: 'Receivable'}];
-    self.type = ['Payable','Receivable'];
+    self.type = [{ name: "Payable"},{name: "Receivable"}]; 
     
     self.chartcode = ko.observable();
     self.chartdescription = ko.observable();
     self.chosenchartType = ko.observable();
 
-    var chartObject = {
-    		chartCode: self.chartcode,
-    		chartDescription: self.chartdescription,
-    		chartType: self.chosenchartType    
+    
+    
+    var userObject = {
+      
+      chartCode: self.chartcode,
+      chartDescription: self.chartdescription,
+      chartType: self.chosenchartType    
     }
     
   //saving the account chart
@@ -27,7 +29,7 @@ function createChartList(charts) {
         $.ajax({
             type: "POST",
             url: "http://localhost:8080/AccountingERPSystem/rest/ChartOfAccounts/Save",
-            data: ko.toJSON(chartObject),
+            data: ko.toJSON(userObject),
             contentType: 'application/json',
             dataType: 'json',
             success: function()
@@ -40,7 +42,6 @@ function createChartList(charts) {
             {
                 alert("Error!!Could not add");
                 //empty properties here
-                console.log(ko.toJSON(chartObject));
                 
             }
         });
